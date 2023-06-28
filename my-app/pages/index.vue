7c55835e4a899ce97c839e0a123adabf382f5bf1
zulfahmidev <developer.zulfahmi@gmail.com>
2023-06-28 19:52:58 +0700

    init project nuxt

---
+++ my-app/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="m-auto bg-primary h-screen text-center text-white flex flex-col justify-center align-center" style="max-width: 425px;">
+    <div class="flex justify-center relative">
+      <img src="/images/border.png" class="absolute" alt="" style="width:13.5rem;top:-27px;left:90px">
+      <div class="w-40 h-40 overflow-hidden rounded-full">
+        <img src="/images/prewed.jpeg" alt="">
+      </div>
+    </div>
+    <div class="mt-3">THE WEDDING OF</div>
+    <div class="font-smg text-5xl my-4">Riza & Sarah</div>
+    <div class="">Kepada Bapak/Ibu/Saudara/I</div>
+    <div class="text-secondary text-xl my-4 font-bold">Keluarga Besar UKM-POLICY</div>
+    <a href="" class="text-primary bg-white mx-auto rounded-full w-48 px-6 py-3 shadow-lg mt-2 inline-block">
+      <!-- <i class="fa fa-fw fa-book-open"></i>  -->
+      <Icon name="fa6-solid:book-open" class="relative mr-1" style="bottom:1px" />
+      Buka Undangan
+    </a>
+  </div>
+</template>
---
+++ my-app/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#D8BE7B',
+        secondary: '#897644',
+      }
+    }
+  },
+  plugins: [],
+}
