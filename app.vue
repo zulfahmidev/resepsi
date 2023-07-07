18ad6dc233954ccd8eae30bf0a68581b7ab04ce0
zulfahmidev <developer.zulfahmi@gmail.com>
2023-07-07 12:28:01 +0700

    memperbaiki bug

--- app.vue
+++ app.vue
@@ -18,12 +18,9 @@
   .font-inter {
     font-family: 'Inter', sans-serif;
   }
-.page-enter-active,
-.page-leave-active {
-  transition: all 0.4s;
-}
-.page-enter-from,
+
 .page-leave-to {
+  transition: all .4s;
   opacity: 0;
   filter: blur(1rem);
 }
